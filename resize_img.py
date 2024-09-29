9e665ec169001ca0cf5ac1ee272aad809c35a851
KorawichInt <korawich.inthamusik@gmail.com>
2024-09-29 21:53:11 +0700

    resize and change all images type to jpg

--- resize_img.py
+++ resize_img.py
@@ -1,32 +1,58 @@
-# from PIL import Image
 # import os
+# from PIL import Image
 
-# image = Image.open('dataset/images_no_resize/00001.jpg')
-# new_image = image.resize((640, 640))
-# new_image.save('dataset/images/00001.jpg')
+# # Define the paths
+# input_folder = 'project_dataset'
+# output_folder = 'project_dataset_resized'
+
+# # Create the output folder if it does not exist
+# os.makedirs(output_folder, exist_ok=True)
+
+# # Loop through each file in the input folder
+# for filename in os.listdir(input_folder):
+#     if filename.endswith('.jpg') or filename.endswith('.png'):
+#         # Open the image file
+#         image_path = os.path.join(input_folder, filename)
+#         image = Image.open(image_path)
+        
+#         # Resize the image
+#         new_image = image.resize((224, 224))
+        
+#         # Save the resized image to the output folder
+#         # new_image_path = os.path.join(output_folder, filename)
+#         # new_image.save(new_image_path)
+#         base_name = os.path.splitext(filename)
+#         # เปิดภาพต้นฉบับ
+#         with Image.open(filename) as img:
+#             # แปลงและบันทึกเป็น JPG
+#             img.convert('RGB').save(f"{base_name}.jpg", "JPEG")
+
+# print("All images have been resized and saved.")
 
 import os
 from PIL import Image
 
 # Define the paths
-input_folder = 'dataset/images_no_resize'
-output_folder = 'dataset/images'
+input_folder = 'project_dataset'
+output_folder = 'project_dataset_resized'
 
 # Create the output folder if it does not exist
 os.makedirs(output_folder, exist_ok=True)
 
 # Loop through each file in the input folder
 for filename in os.listdir(input_folder):
-    if filename.endswith('.jpg'):
+    if filename.endswith('.jpg') or filename.endswith('.png'):
         # Open the image file
         image_path = os.path.join(input_folder, filename)
         image = Image.open(image_path)
         
         # Resize the image
-        new_image = image.resize((640, 640))
-        
-        # Save the resized image to the output folder
-        new_image_path = os.path.join(output_folder, filename)
-        new_image.save(new_image_path)
+        new_image = image.resize((224, 224))
 
-print("All images have been resized and saved.")
+        # Convert the image to RGB and save as .jpg
+        base_name = os.path.splitext(filename)[0]  # Get the filename without extension
+        new_image_path = os.path.join(output_folder, f"{base_name}.jpg")
+        new_image.convert('RGB').save(new_image_path, "JPEG")
+
+print("All images have been resized and saved as .jpg.")
+
