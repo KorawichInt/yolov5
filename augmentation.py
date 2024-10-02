5adae0e57c512a01fd2c219144bd2cf1524c746e
KorawichInt <korawich.inthamusik@gmail.com>
2024-10-03 00:36:47 +0700

    change augmented iamges and labels

--- augmentation.py
+++ augmentation.py
@@ -188,12 +188,12 @@ for filename in os.listdir(input_images_path):
 
         ### Augmentation
         # croped_img, croped_boxes = v2.RandomCrop(size=(224, 224))(image, boxes)
-        rotate_img, rotate_boxes = v2.RandomRotation(degrees=(0, 360))(image, boxes)
-        blured_img, blured_boxes = v2.GaussianBlur(kernel_size=9, sigma=(0.8, 1.2))(image, boxes)
-        brightnessed_img, brightnessed_boxes = v2.ColorJitter(brightness=(1.1,1.3))(image, boxes)
+        rotate_img, rotate_boxes = v2.RandomRotation(degrees=(-45, 45))(image, boxes)
+        blured_img, blured_boxes = v2.GaussianBlur(kernel_size=9, sigma=(1.05, 1.1))(image, boxes)
+        brightnessed_img, brightnessed_boxes = v2.ColorJitter(brightness=(1.05,1.1))(image, boxes)
         darknessed_img, darknessed_boxes = v2.ColorJitter(brightness=(0.7,0.9))(image, boxes)
         horizontal_flip_img, horizontal_flip_boxes = v2.RandomHorizontalFlip(p=1.0)(image, boxes)
-        vertical_flip_img, vertical_flip_boxes = v2.RandomVerticalFlip(p=1.0)(image, boxes)   
+        # vertical_flip_img, vertical_flip_boxes = v2.RandomVerticalFlip(p=1.0)(image, boxes)   
 
         augmentations = {
         #     'croped': (croped_img, croped_boxes, 224, 224),
@@ -202,7 +202,7 @@ for filename in os.listdir(input_images_path):
             'brightnessed': (brightnessed_img, brightnessed_boxes, img_width, img_height),
             'darknessed': (darknessed_img, darknessed_boxes, img_width, img_height),
             'horizontal': (horizontal_flip_img, horizontal_flip_boxes, img_width, img_height),
-            'vertical': (vertical_flip_img, vertical_flip_boxes, img_width, img_height),
+            # 'vertical': (vertical_flip_img, vertical_flip_boxes, img_width, img_height),
         }
 
         for prefix, (aug_img, aug_boxes, aug_width, aug_height) in augmentations.items():
@@ -222,4 +222,4 @@ for filename in os.listdir(input_images_path):
         brightnessed_img.save(os.path.join(output_images_path, f'brightnessed_{filename}'))
         darknessed_img.save(os.path.join(output_images_path, f'darknessed_{filename}'))
         horizontal_flip_img.save(os.path.join(output_images_path, f'horizontal_flip_{filename}'))
-        vertical_flip_img.save(os.path.join(output_images_path, f'vertical_flip_{filename}'))
+        # vertical_flip_img.save(os.path.join(output_images_path, f'vertical_flip_{filename}'))
