a016d70da5aaefdd0375535e310c707d849491a4
SasiCm <creamfridayapril@gmail.com>
2024-10-02 23:36:58 +0700

    delete brightnessed img & labels

--- augmentation.py
+++ augmentation.py
@@ -190,7 +190,7 @@ for filename in os.listdir(input_images_path):
         # croped_img, croped_boxes = v2.RandomCrop(size=(224, 224))(image, boxes)
         # rotate_img, rotate_boxes = v2.RandomRotation(degrees=(0, 360))(image, boxes)
         blured_img, blured_boxes = v2.GaussianBlur(kernel_size=9, sigma=(0.1, 0.5))(image, boxes)
-        brightnessed_img, brightnessed_boxes = v2.ColorJitter(brightness=(1.1,1.1))(image, boxes)
+        # brightnessed_img, brightnessed_boxes = v2.ColorJitter(brightness=(1.1,1.1))(image, boxes)
         darknessed_img, darknessed_boxes = v2.ColorJitter(brightness=(0.7,0.9))(image, boxes)
         horizontal_flip_img, horizontal_flip_boxes = v2.RandomHorizontalFlip(p=1.0)(image, boxes)
         # vertical_flip_img, vertical_flip_boxes = v2.RandomVerticalFlip(p=1.0)(image, boxes)   
@@ -199,7 +199,7 @@ for filename in os.listdir(input_images_path):
         #     'croped': (croped_img, croped_boxes, 224, 224),
             # 'rotated': (rotate_img, rotate_boxes, img_width, img_height),
             'blured': (blured_img, blured_boxes, img_width, img_height),
-            'brightnessed': (brightnessed_img, brightnessed_boxes, img_width, img_height),
+            # 'brightnessed': (brightnessed_img, brightnessed_boxes, img_width, img_height),
             'darknessed': (darknessed_img, darknessed_boxes, img_width, img_height),
             'horizontal': (horizontal_flip_img, horizontal_flip_boxes, img_width, img_height),
             # 'vertical': (vertical_flip_img, vertical_flip_boxes, img_width, img_height),
@@ -219,7 +219,7 @@ for filename in os.listdir(input_images_path):
         # croped_img.save(os.path.join(output_images_path, f'cropped_{filename}'))
         # rotate_img.save(os.path.join(output_images_path, f'rotated_{filename}'))
         blured_img.save(os.path.join(output_images_path, f'blurred_{filename}'))
-        brightnessed_img.save(os.path.join(output_images_path, f'brightnessed_{filename}'))
+        # brightnessed_img.save(os.path.join(output_images_path, f'brightnessed_{filename}'))
         darknessed_img.save(os.path.join(output_images_path, f'darknessed_{filename}'))
         horizontal_flip_img.save(os.path.join(output_images_path, f'horizontal_flip_{filename}'))
         # vertical_flip_img.save(os.path.join(output_images_path, f'vertical_flip_{filename}'))
